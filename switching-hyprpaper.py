f226987f45d1517e910ada44067989d3fb8243f6
SaltyBeggar <saltybeggar@gmail.com>
2026-04-21 15:33:15 -0300

    Hyprland wallpaper autoswitching utility

---
+++ switching-hyprpaper.py
@@ -0,0 +1,20 @@
+#!/bin/python
+import os
+import random
+import time
+import subprocess
+import sys
+
+while True:
+    wallpaperDir = os.getenv("SWITCHING_WALLPAPER_DIR")
+    interval = (float) (sys.argv[1])
+
+    os.chdir(wallpaperDir)
+    files = os.listdir(wallpaperDir)
+    random.shuffle(files)
+    curIndex = 0
+    for img in files:
+        time.sleep(interval)
+        command = f"DP-1, ./{img}"
+        subprocess.run(["hyprctl", "hyprpaper", "wallpaper", command]) 
+
