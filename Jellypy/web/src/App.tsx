e0c2f1cdf6295c05cf34e168c8a6403239691e9a
Salty-Beggar <ivolijuguedes@gmail.com>
2026-04-27 16:49:50 -0300

    Starting Jellypy

---
+++ Jellypy/web/src/App.tsx
@@ -0,0 +1,15 @@
+
+import { Header } from './components/Header/Header'
+import './App.scss'
+import { Body } from './components/Body/Body'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Body/>
+    </>
+  )
+}
+
+export default App
---
+++ Jellypy/web/src/components/Body/Body.tsx
@@ -0,0 +1,8 @@
+
+export function Body() {
+    return (
+        <>
+            
+        </>
+    );
+}
